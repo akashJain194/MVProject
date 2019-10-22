9c7f28154b44ced9dff9117fc99858a5f0672a76
akashJain194 <akashj@gmail.com>
2019-10-22 10:48:57 -0700

    Merge branch 'master' of /Users/akash/IdeaProjects/MVProject with conflicts.

--- src/main.java
+++ src/main.java
@@ -6,19 +6,21 @@ public class main {
 
     }
 
-    public boolean relevant(WordBucket questionWords, WordBucket answerWords) {
+    public boolean relevance(WordBucket questionWords, WordBucket answerWords) {
         int count = 0;
+        double thresholdForRelevance = 0.10;
         ArrayList<WordFreq> qWords = questionWords.getWords();
         ArrayList<WordFreq> aWords = answerWords.getWords();
+
         for (int i = 0; i < aWords.size(); i++) {
+            String aWord = aWords.get(i).getWord();
             for (int j = 0; j < qWords.size(); j++) {
-                String aWord = aWords.get(i).getWord();
-                String qWord = qWords.get(i).getWord();
-                if(aWord.equals(qWord)) count+=aWords.get(i).getFrequency();
+                String qWord = qWords.get(j).getWord();
+                if (aWord.equals(qWord)) count++;
             }
         }
-        double percentOfAnswer = (double)(count) / aWords.size();
-        if(percentOfAnswer >= 0.1) return true;
+        double percentOfAnswer = (double) (count) / aWords.size();
+        if (percentOfAnswer >= thresholdForRelevance) return true;
         else return false;
     }
 
@@ -36,77 +38,80 @@ public class main {
     public static int getSyllablesFromString(String thisWord) {
         ArrayList<String> vowelChains = getVowelChains(thisWord);
         int syllables = vowelChains.size();
-        if(vowelChains.size()==0)return 1;
-        syllables+=syllablesForSilentE(thisWord, vowelChains);
-        syllables+=syllablesForIChains(thisWord, vowelChains);
-        syllables+=syllablesForEd(thisWord, vowelChains);
-        if(thisWord.contains("oolo"))syllables++;
+        if (vowelChains.size() == 0) return 1;
+        syllables += syllablesForSilentE(thisWord, vowelChains);
+        syllables += syllablesForIChains(thisWord, vowelChains);
+        syllables += syllablesForEd(thisWord, vowelChains);
+        if (thisWord.contains("oolo")) syllables++;
 
 
-
-        if(syllables==0)syllables=1;
+        if (syllables == 0) syllables = 1;
         return syllables;
     }
 
     public static int syllablesForEd(String thisWord, ArrayList<String> vowelChains) {
 
-        if(thisWord.length()>5){
-            if(!containsVowel(thisWord.substring(thisWord.length()-5, thisWord.length()-2))&&thisWord.substring(thisWord.length()-2).equals("ed"))return 0;
+        if (thisWord.length() > 5) {
+            if (!containsVowel(thisWord.substring(thisWord.length() - 5, thisWord.length() - 2)) && thisWord.substring(thisWord.length() - 2).equals("ed"))
+                return 0;
         }
-        if(thisWord.substring(thisWord.length()-2).equals("ed"))return -1;
+        if (thisWord.substring(thisWord.length() - 2).equals("ed")) return -1;
         return 0;
     }
 
     private static int syllablesForIChains(String thisWord, ArrayList<String> vowelChains) {
-        if(thisWord.length()>4) {
+        if (thisWord.length() > 4) {
             if (thisWord.substring(thisWord.length() - 3).equals("ier")) return 1;
         }
-        if(thisWord.contains("ia")&&!thisWord.contains("cia"))return 1;
-        if(thisWord.contains("iest"))return 1;
-        if(thisWord.contains("iu"))return 1;
+        if (thisWord.contains("ia") && !thisWord.contains("cia")) return 1;
+        if (thisWord.contains("iest")) return 1;
+        if (thisWord.contains("iu")) return 1;
         return 0;
     }
 
     private static int syllablesForSilentE(String thisWord, ArrayList<String> vowelChains) {
         int change = 0;
-        if(thisWord.length()>4) {
-            if(vowelChains.size()>1) {
+        if (thisWord.length() > 4) {
+            if (vowelChains.size() > 1) {
                 // if (thisWord.substring(3, 4).equals("e") && vowelChains.get(1).equals("e"))change--;
             }
             String substringBeforeEnd = thisWord.substring(thisWord.length() - 3, thisWord.length() - 1);
             if (thisWord.length() > 4 && !containsVowel(substringBeforeEnd) && !substringBeforeEnd.equals("th"))
                 return change;
         }
-        if(thisWord.substring(thisWord.length()-1).equals("e")&&vowelChains.get(vowelChains.size()-1).equals("e"))change--;
+        if (thisWord.substring(thisWord.length() - 1).equals("e") && vowelChains.get(vowelChains.size() - 1).equals("e"))
+            change--;
         return change;
     }
 
     private static ArrayList<String> getVowelChains(String thisWord) {
-        ArrayList<String> vowelChains= new ArrayList<>();
+        ArrayList<String> vowelChains = new ArrayList<>();
         String thisVowelChain = "";
-        while(thisWord.length()>0){
+        while (thisWord.length() > 0) {
             String letter = thisWord.substring(0, 1);
-            if(isVowel(letter)){
-                thisVowelChain+=letter;
-            }else{
-                if(!thisVowelChain.equals(""))vowelChains.add(thisVowelChain);
-                thisVowelChain="";
+            if (isVowel(letter)) {
+                thisVowelChain += letter;
+            } else {
+                if (!thisVowelChain.equals("")) vowelChains.add(thisVowelChain);
+                thisVowelChain = "";
             }
-            thisWord=thisWord.substring(1);
+            thisWord = thisWord.substring(1);
         }
-        if(!thisVowelChain.equals(""))vowelChains.add(thisVowelChain);
+        if (!thisVowelChain.equals("")) vowelChains.add(thisVowelChain);
         return vowelChains;
     }
-    private static boolean containsVowel(String thisString){
+
+    private static boolean containsVowel(String thisString) {
         for (int i = 0; i < thisString.length(); i++) {
-            String letter = thisString.substring(i, i+1);
-            if(isVowel(letter))return true;
+            String letter = thisString.substring(i, i + 1);
+            if (isVowel(letter)) return true;
         }
         return false;
     }
+
     private static boolean isVowel(String letter) {
         String vowels = "aeiouy";
-        if(vowels.contains(letter))return true;
+        if (vowels.contains(letter)) return true;
         return false;
     }
 
