6ed2cf2a9076601d9c15178ab98933d313bf1f3e
Veeryan <bhatiaveeryan@gmail.com>
2019-10-24 10:34:39 -0700

    wrote the methods (in doc class): -get formal percent -get informal percent

--- src/main.java
+++ src/main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
-    public ArrayList<String> formal_words, informal_words;
+    public static ArrayList<String> formal_words, informal_words;
     public static void main(String[] args) {
 
     } //TODO: Add a parse file method so we can parse the datafiles
